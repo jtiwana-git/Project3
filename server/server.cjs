693d3196682f998ade505ec553873fbaf6f13060
Jagdeep Tiwana <jtiwana@hotmail.com>
2025-01-22 14:00:01 +0000

    changes to file codes

--- server/server.cjs
+++ server/server.cjs
@@ -2,6 +2,7 @@
     Description: This file is the entry point for our application. It is responsible for setting up the server and connecting to the database.
     File: /server/server.js
     Created Date: 21/01/2025
+    Modified Date: 22/01/2025
     Author: Jagdeep Tiwana
  */
 
@@ -10,19 +11,3 @@ const fs = require("fs");
 // express is a web framework for Node.js
 const express = require("express");
 const app = express();
-
-app.get("/", (req, res) => {
-  res.send("<h1>Hello World</h1>");
-});
-
-app.get("/about", (req, res) => {
-  res.send("<h1>About us</h1>");
-});
-
-app.use((req, res, next) => {
-  res.status(404).send("<h1>404 Page Not Found</h1>");
-});
-
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
-});
