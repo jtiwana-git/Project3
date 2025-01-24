c97b20c58275179014b2a2475d528cb6103c2f0c
Jagdeep Tiwana <jtiwana@hotmail.com>
2025-01-24 19:14:36 +0000

    Created login page with styles

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,7 +1,25 @@
-import React from "react";
-
+import "./login.scss";
 const Login = () => {
-  return <div></div>;
+  return (
+    <div className="login">
+      <div className="card">
+        <div className="left">
+          <h1>Bloggers</h1>
+          <p>Bloggers, where bloggers can posts and comments on other posts</p>
+          <span>Don't have an account?</span>
+          <button>Sign up!</button>
+        </div>
+        <div className="right">
+          <h1>Login</h1>
+          <form>
+            <input type="text" placeholder="Username" />
+            <input type="password" placeholder="Password" />
+            <button>Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
